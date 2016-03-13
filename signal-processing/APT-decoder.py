5887b1db23dbb2fedc2c27eaa9ce69aa5c6d39d2
Jarmo Kivekas <jarmo.kivekas@guttula.com>
2016-03-13 16:32:20 +0200

    decode all recording usign gnuradio

---
+++ signal-processing/APT-decoder.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Top Block
+# Generated: Sun Mar 13 15:05:59 2016
+##################################################
+
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+import sys
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 48000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.rational_resampler_xxx_1 = filter.rational_resampler_fff(
+                interpolation=4160,
+                decimation=4800,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=9600,
+                decimation=samp_rate,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.blocks_wavfile_source_0 = blocks.wavfile_source(sys.argv[1], False)
+        self.blocks_skiphead_0 = blocks.skiphead(gr.sizeof_float*1, 1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((255, ))
+        self.blocks_keep_one_in_n_1 = blocks.keep_one_in_n(gr.sizeof_float*1, 2)
+        self.blocks_keep_one_in_n_0 = blocks.keep_one_in_n(gr.sizeof_float*1, 2)
+        self.blocks_float_to_uchar_0 = blocks.float_to_uchar()
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, sys.argv[2], False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
+        self.band_pass_filter_0 = filter.fir_filter_fff(1, firdes.band_pass(
+        	1, samp_rate, 500, 4200, 2000, firdes.WIN_HAMMING, 6.76))
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_wavfile_source_0, 0), (self.band_pass_filter_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_skiphead_0, 0))
+        self.connect((self.blocks_skiphead_0, 0), (self.blocks_keep_one_in_n_1, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_keep_one_in_n_0, 0))
+        self.connect((self.blocks_keep_one_in_n_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_keep_one_in_n_1, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.rational_resampler_xxx_1, 0))
+        self.connect((self.rational_resampler_xxx_1, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_float_to_uchar_0, 0))
+        self.connect((self.blocks_float_to_uchar_0, 0), (self.blocks_file_sink_0, 0))
+
+
+# QT sink close method reimplementation
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, 500, 4200, 2000, firdes.WIN_HAMMING, 6.76))
+
+if __name__ == '__main__':
+    import ctypes
+    import os
+    if os.name == 'posix':
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = top_block()
+    tb.Start(True)
+    tb.Wait()
+
